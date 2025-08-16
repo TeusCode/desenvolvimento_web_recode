2f7ab21c85ada6f0173c1cdc8acbf5368ae58251
Mateus Vilaça <mateusdasilvavilaca@gmail.com>
2025-08-16 14:32:55 -0300

    Update script.js

--- objetos/script.js
+++ objetos/script.js
@@ -10,14 +10,31 @@ Método → Função dentro do objeto (define ações).
 
 //Como criar um objeto?
 
-let pessoa = {
-    atributo: 'nome',
+const pessoa = {
+    nome: 'Mateus',
     idade: 12,
     altura: 1.86,
     sexo: 'masc',
+    comer: function () {
+        console.log(pessoa.nome + ' ' + 'comeu uma maça')
+    }
 };
 
 //Como adicionar novo atributo:
 pessoa['cor'] = 'pardo';
 //Como atualizar um valor de um atributo:
 pessoa.idade = 15;
+
+//Como chamar um método?
+pessoa.comer()
+
+//Como criar classes?
+class Pessoa {
+    constructor(v1, v2, v3) {
+        this.nome = v1;
+        this.idade = v2;
+        this.altura = v3;
+    }
+}
+let mateus = new Pessoa("mateus", 21, 1.86)
+console.log(mateus)
